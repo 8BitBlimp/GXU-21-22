e3a873e29dd1cadb23c5516f65cc6a0b80033027
LAPTOP-HKFTEVNJ\Anders Bruun Nørring <hej@abn.dk>
2022-01-17 20:39:54 +0100

    ny chikane og slider bevægelse

--- PingPong/pingpong.py
+++ PingPong/pingpong.py
@@ -6,17 +6,32 @@ screen = pygame.display.set_mode([500, 500])
 
 kPosX = 200
 kPosY = 300
+sliderPosX = 300
 
 kSpeed = 0.05
 
 retHoejre = True
 retNed = True
 
+sliderRetHoejre = False
+sliderRetVenstre = False
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                sliderRetHoejre = True
+            if event.key == pygame.K_LEFT:
+                sliderRetVenstre = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                sliderRetHoejre = False
+            if event.key == pygame.K_LEFT:
+                sliderRetVenstre = False
+
 
     screen.fill((255, 255, 255))
 
@@ -26,7 +41,34 @@ while running:
     sort = pygame.draw.rect(screen, (0,0,0),  (50,450,400,2))
     kasse = pygame.draw.rect(screen, (50,50,50), (kPosX,kPosY,30,30))
 
-    chikane1 = pygame.draw.rect(screen, (100,0,0),  (150,245,200,10))
+    chikaneovre = pygame.draw.rect(screen, (255,0,0),  (152,245,196,2))
+    chikanenedre = pygame.draw.rect(screen, (255,0,0),  (152,255,196,2))
+    chikanehoejre = pygame.draw.rect(screen, (0,255,0),  (349,246,2,10))
+    chikanevenstre = pygame.draw.rect(screen, (0,255,0),  (150,246,2,10))
+
+    slider = pygame.draw.rect(screen, (0,0,0),(sliderPosX,430,100,5))
+
+    if(sliderRetHoejre):
+        sliderPosX = sliderPosX + 0.1
+
+    if(sliderRetVenstre):
+        sliderPosX = sliderPosX - 0.1
+
+    if(kasse.colliderect(slider)):
+        retNed = False
+
+    if(kasse.colliderect(chikaneovre)):
+        retNed = False
+    
+    if(kasse.colliderect(chikanenedre)):
+        retNed = True
+    
+    if(kasse.colliderect(chikanehoejre)):
+        retHoejre = True
+    
+    if(kasse.colliderect(chikanevenstre)):
+        retHoejre = False
+
 
     if(retHoejre):
         kPosX = kPosX + kSpeed
@@ -48,7 +90,7 @@ while running:
         retHoejre = False
 
     if(kasse.colliderect(sort)):
-        retNed = False
+        kSpeed = 0
 
 
     pygame.display.flip()
